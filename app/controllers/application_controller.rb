ea9514aeaf4daeab055915c47e157b7a71eb8c87
AnanthaRohan <anantharohan299@gmail.com>
2022-10-05 17:11:39 +0000

    First rails project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,51 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    index = rand(3)
+
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.at(index)
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/rock.html.erb" })
+  end
+
+  def play_paper
+    index = rand(3)
+
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.at(index)
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/paper.html.erb" })
+  end
+
+  def play_scissors
+    index = rand(3)
+
+    moves = ["rock", "paper", "scissors"]
+    @comp_move = moves.at(index)
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/scissors.html.erb" })
+  end
 end
